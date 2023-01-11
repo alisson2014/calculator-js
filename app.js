2d2daf8cfd7089547e73365fad0ba14b43344e48
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-11 14:41:14 -0300

    finish script

--- app.js
+++ app.js
@@ -22,17 +22,10 @@ const pendingOperation = () => operator !== undefined
 
 const calc = () => {
   if (pendingOperation()) {
-    const currentNumber = parseFloat(display.textContent)
+    const currentNumber = parseFloat(display.textContent.replace(",", "."))
     newNumber = true
-    if (operator == "+") {
-      displayUpdate(previousNumber + currentNumber)
-    } else if (operator == "-") {
-      displayUpdate(previousNumber - currentNumber)
-    } else if (operator == "*") {
-      displayUpdate(previousNumber * currentNumber)
-    } else if (operator == "/") {
-      displayUpdate(previousNumber / currentNumber)
-    }
+    const result = eval(`${previousNumber}${operator}${currentNumber}`)
+    displayUpdate(result)
   }
 }
 
@@ -51,7 +44,7 @@ const selectOperator = (event) => {
     calc()
     newNumber = true
     operator = event.target.textContent
-    previousNumber = parseFloat(display.textContent)
+    previousNumber = parseFloat(display.textContent.replace(",", "."))
   }
 }
 
@@ -59,3 +52,76 @@ numbers.forEach((number) => number.addEventListener("click", insertNumber))
 operators.forEach((operator) =>
   operator.addEventListener("click", selectOperator)
 )
+
+const activeEqual = () => {
+  calc()
+  operator = undefined
+}
+
+const cleanDisplay = () => (display.textContent = "")
+
+const cleanCalc = () => {
+  cleanDisplay()
+  operator = undefined
+  newNumber = true
+  previousNumber = undefined
+}
+
+const removeLastData = () =>
+  (display.textContent = display.textContent.slice(0, -1))
+
+const reverseSign = () => {
+  newNumber = true
+  displayUpdate(display.textContent * -1)
+}
+
+const decimalExists = () => display.textContent.indexOf(",") !== -1
+
+const existValue = () => display.textContent.length > 0
+
+const insertDecimal = () => {
+  if (!decimalExists()) {
+    if (existValue()) {
+      displayUpdate(".")
+    } else {
+      displayUpdate("0.")
+    }
+  }
+}
+
+const keyboardMap = {
+  0: "key0",
+  1: "key1",
+  2: "key2",
+  3: "key3",
+  4: "key4",
+  5: "key5",
+  6: "key6",
+  7: "key7",
+  8: "key8",
+  9: "key9",
+  0: "key0",
+  0: "key0",
+}
+
+const mapKeyboard = (event) => {
+  const key = event.key
+  const allowedKey = () => Object.keys(keyboardMap).indexOf(key) !== -1
+
+  if (allowedKey()) document.getElementById(keyboardMap[key]).click()
+}
+
+document.addEventListener("keydown", mapKeyboard)
+
+window.document
+  .getElementById("decimal")
+  .addEventListener("click", insertDecimal)
+window.document.getElementById("equal").addEventListener("click", activeEqual)
+window.document
+  .getElementById("cleanDisplay")
+  .addEventListener("click", cleanDisplay)
+window.document.getElementById("cleanCalc").addEventListener("click", cleanCalc)
+window.document
+  .getElementById("backspace")
+  .addEventListener("click", removeLastData)
+window.document.getElementById("reverse").addEventListener("click", reverseSign)
