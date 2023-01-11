d77164914eb80892401b048b97a671fa11478d15
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-11 16:02:31 -0300

    code review

--- app.js
+++ app.js
@@ -1,84 +1,97 @@
 /* Switch button */
-const themeToggleBtn = document.querySelector(".theme-toggler")
-const calculator = document.querySelector(".calculator")
-const toggleIcon = document.querySelector(".toggler-icon")
-let isDark = true
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
 themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark")
-  themeToggleBtn.classList.toggle("active")
-  isDark = !isDark
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
 }
 
 /* Calculator */
-const display = window.document.querySelector("div#display")
-const numbers = window.document.querySelectorAll("[id*=key]")
-const operators = window.document.querySelectorAll("[id*=Operator]")
+const display = window.document.querySelector("div#display");
+const numbers = window.document.querySelectorAll("[id*=key]");
+const operators = window.document.querySelectorAll("[id*=Operator]");
 
-let newNumber = true
-let operator
-let previousNumber
+let newNumber = true;
+let operator;
+let previousNumber;
 
-const pendingOperation = () => operator !== undefined
+const pendingOperation = () => operator !== undefined;
 
+//Calculo realizado se houver operação pendente
 const calc = () => {
   if (pendingOperation()) {
-    const currentNumber = parseFloat(display.textContent.replace(",", "."))
-    newNumber = true
-    const result = eval(`${previousNumber}${operator}${currentNumber}`)
-    displayUpdate(result)
+    const currentNumber = parseFloat(display.textContent.replace(",", "."));
+    newNumber = true;
+    const result = eval(`${previousNumber}${operator}${currentNumber}`);
+    displayUpdate(result.toFixed(3));
   }
 }
 
+//Atualizar o display quando adicionar numeros
 const displayUpdate = (text) => {
   if (newNumber) {
-    display.textContent = text
-    newNumber = false
+    display.textContent = text;
+    newNumber = false;
   } else {
-    display.textContent += text
+    display.textContent += text;
   }
 }
 
-const insertNumber = (event) => displayUpdate(event.target.textContent)
+//inserir numero quando o display receber um novo numero
+const insertNumber = (event) => displayUpdate(event.target.textContent);
 const selectOperator = (event) => {
+  //Se for adicionao um novo numero execute o calculo
   if (!newNumber) {
-    calc()
-    newNumber = true
-    operator = event.target.textContent
-    previousNumber = parseFloat(display.textContent.replace(",", "."))
+    calc();
+    newNumber = true;
+    operator = event.target.textContent;
+    previousNumber = parseFloat(display.textContent.replace(",", "."));
   }
 }
 
-numbers.forEach((number) => number.addEventListener("click", insertNumber))
+//forEach para pegar vários dados
+numbers.forEach((number) => number.addEventListener("click", insertNumber));
 operators.forEach((operator) =>
   operator.addEventListener("click", selectOperator)
-)
+);
 
+//execute o calculo quando clicado em igual
 const activeEqual = () => {
-  calc()
-  operator = undefined
-}
+  calc();
+  operator = undefined;
+};
 
-const cleanDisplay = () => (display.textContent = "")
+//Limpar display
+const cleanDisplay = () => (display.textContent = "");
 
+//Limpar calculo (limpa o display e os dados ja inseridos)
 const cleanCalc = () => {
-  cleanDisplay()
-  operator = undefined
-  newNumber = true
-  previousNumber = undefined
+  cleanDisplay();
+  operator = undefined;
+  newNumber = true;
+  previousNumber = undefined;
 }
 
+//Remove o ultimo dado (backspace)
 const removeLastData = () =>
-  (display.textContent = display.textContent.slice(0, -1))
+  (display.textContent = display.textContent.slice(0, -1));
 
+//Reverte o sinal da operação
 const reverseSign = () => {
   newNumber = true
   displayUpdate(display.textContent * -1)
-}
+};
 
-const decimalExists = () => display.textContent.indexOf(",") !== -1
+//Verifica se existe um numero decimal no display
+const decimalExists = () => display.textContent.indexOf(",") !== -1;
 
-const existValue = () => display.textContent.length > 0
+//Verifica se existe um valor no display
+const existValue = () => display.textContent.length > 0;
 
+//Verifica se não existe um decimal
 const insertDecimal = () => {
   if (!decimalExists()) {
     if (existValue()) {
@@ -87,8 +100,9 @@ const insertDecimal = () => {
       displayUpdate("0.")
     }
   }
-}
+};
 
+//Objeto com o mapa do teclado
 const keyboardMap = {
   0: "key0",
   1: "key1",
@@ -100,28 +114,32 @@ const keyboardMap = {
   7: "key7",
   8: "key8",
   9: "key9",
-  0: "key0",
-  0: "key0",
-}
+  "/": "divisionOperator",
+  "*": "multiplicationOperator",
+  "-": "subtractionOperator",
+  "+": "addOperator",
+  "=": "equal",
+  Enter: "equal",
+  Backspace: "backspace",
+  c: "cleanDisplay",
+  Escape: "cleanCalc",
+  ",": "decimal",
+};
 
+//Mapeamento para o teclado funcionar simultaneamente aos clicks
 const mapKeyboard = (event) => {
   const key = event.key
   const allowedKey = () => Object.keys(keyboardMap).indexOf(key) !== -1
-
   if (allowedKey()) document.getElementById(keyboardMap[key]).click()
-}
+};
 
-document.addEventListener("keydown", mapKeyboard)
+//Adicionando eventos ao teclado (keydown)
+document.addEventListener("keydown", mapKeyboard);
 
-window.document
-  .getElementById("decimal")
-  .addEventListener("click", insertDecimal)
-window.document.getElementById("equal").addEventListener("click", activeEqual)
-window.document
-  .getElementById("cleanDisplay")
-  .addEventListener("click", cleanDisplay)
-window.document.getElementById("cleanCalc").addEventListener("click", cleanCalc)
-window.document
-  .getElementById("backspace")
-  .addEventListener("click", removeLastData)
-window.document.getElementById("reverse").addEventListener("click", reverseSign)
+//adicionando eventos a DOM
+document.getElementById("decimal").addEventListener("click", insertDecimal);
+document.getElementById("equal").addEventListener("click", activeEqual);
+document.getElementById("cleanDisplay").addEventListener("click", cleanDisplay);
+document.getElementById("cleanCalc").addEventListener("click", cleanCalc);
+document.getElementById("backspace").addEventListener("click", removeLastData);
+document.getElementById("reverse").addEventListener("click", reverseSign);
