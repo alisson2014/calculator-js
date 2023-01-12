c91bfaff073bf0986c2cb405c7a6db6adf1c4835
alisson2014 <almeidaalisson2014@gmail.com>
2023-01-12 09:10:19 -0300

    Separating the code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,3 @@
-/* Switch button */
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-}
-
 /* Calculator */
 const display = window.document.querySelector("div#display");
 const numbers = window.document.querySelectorAll("[id*=key]");
---
+++ js/switch.js
@@ -0,0 +1,10 @@
+/* Switch button */
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+}
